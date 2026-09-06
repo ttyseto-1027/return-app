a6784936d2c7ff5fb24a1ba3ac1cec02ed6cee17
ttyseto-1027 <tty.seto1027@gmail.com>
2026-09-06 14:19:57 +0900

    Refresh cache for medication history layout

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='return-v5';
+const CACHE='return-v6';
 const ASSETS=['./','./index.html','./history.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
